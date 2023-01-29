ffb131914f0406f6af86fddc1736d9a3f216e933
TimoHenderson <tjhenderson2@gmail.com>
2023-01-29 19:19:53 +0000

    Refactor find_pet_by_name

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -28,9 +28,10 @@ def get_pets_by_breed(pet_shop, breed):
 
 
 def find_pet_by_name(pet_shop, name):
-    for pet in pet_shop["pets"]:
-        if pet["name"] == name:
-            return pet
+    # for pet in pet_shop["pets"]:
+    #     if pet["name"] == name:
+    #         return pet
+    return next((pet for pet in pet_shop["pets"] if pet["name"] == name), None)
 
 
 def remove_pet_by_name(pet_shop, name):
